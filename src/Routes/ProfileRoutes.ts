4915433560bd9503b6182eed840c1a4428846351
jonathan_baraza <jbaraza46@gmail.com>
2023-05-07 20:21:32 +0300

    photo upload route

--- src/Routes/ProfileRoutes.ts
+++ src/Routes/ProfileRoutes.ts
@@ -1,9 +1,14 @@
 import express, { Router } from "express";
-import { fetchProfile, updateProfile } from "../controllers/Profile";
+import {
+  fetchProfile,
+  updateProfile,
+  updateProfilePhoto,
+} from "../controllers/Profile";
 import JWTGuard from "../guards/JWTGuard";
 const router: Router = express.Router();
 
 router.get("/", JWTGuard, fetchProfile);
 router.post("/update", JWTGuard, updateProfile);
+router.post("/update-photo", JWTGuard, updateProfilePhoto);
 
 export default router;
