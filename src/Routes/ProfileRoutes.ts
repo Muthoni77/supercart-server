8a11d20c462e01f7af0e3b13ec51018140c11130
jonathan_baraza <jbaraza46@gmail.com>
2023-05-07 21:57:59 +0300

    jwt guard and multer middleware chainig

--- src/Routes/ProfileRoutes.ts
+++ src/Routes/ProfileRoutes.ts
@@ -5,10 +5,16 @@ import {
   updateProfilePhoto,
 } from "../controllers/Profile";
 import JWTGuard from "../guards/JWTGuard";
+import uploader from "../utils/multer";
 const router: Router = express.Router();
 
 router.get("/", JWTGuard, fetchProfile);
 router.post("/update", JWTGuard, updateProfile);
-router.post("/update-photo", JWTGuard, updateProfilePhoto);
+router.post(
+  "/update-photo",
+  JWTGuard,
+  uploader.single("file"),
+  updateProfilePhoto
+);
 
 export default router;
