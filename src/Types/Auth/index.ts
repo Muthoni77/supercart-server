d81f74bc82ccca57302caba2dd3509bf4e0f3666
jonathan_baraza <jbaraza46@gmail.com>
2023-05-31 20:06:01 +0300

    string additional types for id

--- src/Types/Auth/index.ts
+++ src/Types/Auth/index.ts
@@ -26,7 +26,7 @@ export interface UserType extends Document {
 }
 
 export interface JWTPayloadType {
-  id: Types.ObjectId;
+  id: Types.ObjectId | string;
   email: string;
 }
 export interface JWTExpiresInType {
