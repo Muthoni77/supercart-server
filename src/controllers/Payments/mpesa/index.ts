c8180f507abeac956eb6cec0a63acd72157b37ed
jonathan_baraza <jbaraza46@gmail.com>
2023-05-31 20:07:45 +0300

    payments controller init

---
+++ src/controllers/Payments/mpesa/index.ts
@@ -0,0 +1,9 @@
+import { Response } from "express";
+import { CustomRequest } from "../../../Types/Auth";
+
+export const handleMpesaCheckout = async (
+  req: CustomRequest,
+  res: Response
+) => {
+  res.status(200).json({ message: "Handling checkout" });
+};
