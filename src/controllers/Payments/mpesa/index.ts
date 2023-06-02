367acc3b7fcbb80df4233fcbda69d32b40315373
jonathan_baraza <jbaraza46@gmail.com>
2023-06-02 22:43:44 +0300

    handle error cases

--- src/controllers/Payments/mpesa/index.ts
+++ src/controllers/Payments/mpesa/index.ts
@@ -1,36 +1,43 @@
-import { Response } from "express";
+import { NextFunction, Response } from "express";
 import { CustomRequest } from "../../../Types/Auth";
 import axios from "axios";
+import { AccessTokenType } from "../../../Types/Payments/Mpesa";
 
 const mpesaEndpoint = process.env.MPESA_ENDPOINT;
 
 export const handleMpesaCheckout = async (
   req: CustomRequest,
-  res: Response
+  res: Response,
+  next: NextFunction
 ) => {
-  await generateAccessToken();
-  res.status(200).json({ message: "Handling checkout" });
+  const accessToken = await generateAccessToken(next);
+  res.status(200).json({ message: "Handling checkout", token: accessToken });
 };
 
-export const generateAccessToken = async () => {
-  const consumerKey = process.env.MPESA_CONSUMER_KEY;
-  const consumerSecret = process.env.MPESA_CONSUMER_SECRET;
+export const generateAccessToken = async (
+  next: NextFunction
+): Promise<string | null> => {
+  try {
+    const consumerKey = process.env.MPESA_CONSUMER_KEY;
+    const consumerSecret = process.env.MPESA_CONSUMER_SECRET;
 
-  const encodedString = Buffer.from(
-    `${consumerKey}:${consumerSecret}`,
-    "utf-8"
-  ).toString("base64");
+    const encodedString = Buffer.from(
+      `${consumerKey}:${consumerSecret}`,
+      "utf-8"
+    ).toString("base64");
 
-  const response = await axios({
-    method: "get",
-    url: mpesaEndpoint,
-    headers: {
-      Authorization: `Basic ${encodedString}`,
-    },
-  });
+    const response = await axios({
+      method: "get",
+      url: mpesaEndpoint,
+      headers: {
+        Authorization: `Basic ${encodedString}`,
+      },
+    });
 
-  console.log("response");
-  console.log(response);
-  console.log("response data");
-  console.log(response.data);
+    const { access_token, expires_in }: AccessTokenType = response.data;
+    return access_token;
+  } catch (error) {
+    next(error);
+    return null;
+  }
 };
