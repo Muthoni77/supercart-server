b61758ddffba19ac924a0c7108e01d4c3fbf4f29
jonathan_baraza <jbaraza46@gmail.com>
2023-06-04 13:51:10 +0300

    mpesa callback route

--- src/Routes/payments/mpesa/MpesaRoutes.ts
+++ src/Routes/payments/mpesa/MpesaRoutes.ts
@@ -1,8 +1,12 @@
 import Express, { Router } from "express";
-import { handleMpesaCheckout } from "../../../controllers/Payments/mpesa";
+import {
+  handleMpesaCheckout,
+  handleMpesaCallback,
+} from "../../../controllers/Payments/mpesa";
 import JWTGuard from "../../../guards/JWTGuard";
 const router: Router = Express.Router();
 
 router.post("/checkout", JWTGuard, handleMpesaCheckout);
+router.post("/callback", handleMpesaCallback);
 
 export default router;
--- src/controllers/Payments/mpesa/index.ts
+++ src/controllers/Payments/mpesa/index.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Response } from "express";
+import { NextFunction, Response, Request } from "express";
 import { CustomRequest } from "../../../Types/Auth";
 import axios from "axios";
 import {
@@ -12,10 +12,37 @@ import {
   getTokenPassword,
 } from "../../../utils/payments/mpesa";
 
+//Environment Variables
 const BusinessShortCode = process.env.MPESA_BUSINESS_SHORTCODE!;
 const PassKey = process.env.MPESA_PASSKEY!;
 const CallBackURL = process.env.MPESA_CALLBACK_URL!;
 
+//Generating the authorization token
+export const generateAccessToken = async (
+  next: NextFunction
+): Promise<string | null> => {
+  try {
+    const mpesaEndpoint = process.env.MPESA_TOKEN_ENDPOINT;
+
+    const encodedString = await getTokenPassword();
+
+    const response = await axios({
+      method: "get",
+      url: mpesaEndpoint,
+      headers: {
+        Authorization: `Basic ${encodedString}`,
+      },
+    });
+
+    const { access_token, expires_in }: AccessTokenType = response.data;
+    return access_token;
+  } catch (error) {
+    next(error);
+    return null;
+  }
+};
+
+//sending the STK push
 export const handleMpesaCheckout = async (
   req: CustomRequest,
   res: Response,
@@ -42,7 +69,7 @@ export const handleMpesaCheckout = async (
       PartyB: BusinessShortCode,
       PhoneNumber,
       CallBackURL,
-      AccountReference: "SuperCart Checkout",
+      AccountReference: "SuperCart online store payment",
       TransactionDesc: "SuperCart online store checkout",
     };
 
@@ -66,26 +93,21 @@ export const handleMpesaCheckout = async (
   }
 };
 
-export const generateAccessToken = async (
+//handling callback response from Mpesa
+export const handleMpesaCallback = async (
+  req: Request,
+  res: Response,
   next: NextFunction
-): Promise<string | null> => {
+) => {
   try {
-    const mpesaEndpoint = process.env.MPESA_TOKEN_ENDPOINT;
+    console.log("Callback response from Mpesa is here");
+    console.log(req.body?.stkCallback?.ResultDesc);
+    console.log(req.body);
 
-    const encodedString = await getTokenPassword();
-
-    const response = await axios({
-      method: "get",
-      url: mpesaEndpoint,
-      headers: {
-        Authorization: `Basic ${encodedString}`,
-      },
-    });
-
-    const { access_token, expires_in }: AccessTokenType = response.data;
-    return access_token;
+    res
+      .status(200)
+      .json({ message: req.body?.stkCallback?.ResultDesc, body: req.body });
   } catch (error) {
     next(error);
-    return null;
   }
 };
