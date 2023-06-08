038150e27668b4d68691618854b3eaf49ca29818
jonathan_baraza <jbaraza46@gmail.com>
2023-06-08 21:11:41 +0300

    params

--- src/utils/logData.ts
+++ src/utils/logData.ts
@@ -1,7 +1,8 @@
 import fs from "fs";
 import path from "path";
+import { LogDataBodyType } from "../Types/Utils";
 
-export const logData = async () => {
+export const logData = async ({ filePath, content }: LogDataBodyType) => {
   fs.writeFile(filePath, content, { flag: "a" }, (err: any) => {
     if (err) {
       console.error("An error occurred while writing to the file:", err);
